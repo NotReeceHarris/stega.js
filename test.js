7ad656c16354bc24be64eeef9fde82fa3b7e1b4b
Reece Harris <reeceharris@email.com>
2023-08-29 11:54:22 +0100

    modified:   encode.png 	modified:   test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const pixelAES = require('./index.js');
 
 (async () => {
     const filename = 'encode.png'
-    const originalText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas pharetra. Purus in mollis nunc sed id semper risus. Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Tellus elementum sagittis vitae et leo duis ut. Augue eget arcu dictum varius duis at consectetur lorem. Nunc sed blandit libero volutpat sed cras. In nibh mauris cursus mattis. Nunc mattis enim ut tellus elementum sagittis vitae et. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Semper auctor neque vitae tempus quam pellentesque nec nam. Eu turpis egestas pretium aenean pharetra. At imperdiet dui accumsan sit. Cras ornare arcu dui vivamus arcu felis bibendum ut tristique. In cursus turpis massa tincidunt dui ut ornare.';
+    const originalText = 'Hello World!';
 
     const keys = {
         key: '5bb2ee6d01196b2523bf4659582dd98c60cc1fbf99b891c2cc2a01ff50761f83',
